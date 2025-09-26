8ef81f857a1ffa74c2a52dc83791fe3e702959c2
Andriy Yavorskiy <aiavorskii@gmail.com>
2025-09-26 15:54:58 +0300

    removing unused code

--- example/PayCcTokenExample.php
+++ example/PayCcTokenExample.php
@@ -1,28 +1,22 @@
 <?php
 
 use Centrobill\Sdk\Entity\Consumer;
-use Centrobill\Sdk\Entity\PaymentSource\PaymentSourceCard;
 use Centrobill\Sdk\Entity\PaymentSource\PaymentSourceCcToken;
 use Centrobill\Sdk\Entity\PaymentUrl;
 use Centrobill\Sdk\Entity\Sku;
 use Centrobill\Sdk\Entity\Sku\Url;
 use Centrobill\Sdk\Http\Client;
 use Centrobill\Sdk\Http\Request\PayRequest;
-use Centrobill\Sdk\ValueObject\Cvv;
 use Centrobill\Sdk\ValueObject\Email;
 use Centrobill\Sdk\ValueObject\EmulateCode;
-use Centrobill\Sdk\ValueObject\ExpirationMonth;
-use Centrobill\Sdk\ValueObject\ExpirationYear;
 use Centrobill\Sdk\ValueObject\ExternalId;
 use Centrobill\Sdk\ValueObject\FirstName;
 use Centrobill\Sdk\ValueObject\Ip;
 use Centrobill\Sdk\ValueObject\LastName;
 use Centrobill\Sdk\ValueObject\Mid;
-use Centrobill\Sdk\ValueObject\Number;
 use Centrobill\Sdk\ValueObject\RequestId;
 use Centrobill\Sdk\ValueObject\Sku\Name;
 use Centrobill\Sdk\ValueObject\Url as UrlValue;
-use Centrobill\Sdk\ValueObject\Value;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/http-client.inc.php';
